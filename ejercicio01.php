2d78113dad025be4f698b260efb8873de5c3c4bb
Cristian Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2024-06-11 14:31:35 -0600

    Add files via upload

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,28 +1,33 @@
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css">
-</head>
-<form method="POST" name="AlejandroVillegas">
-	<h1>Ingresar 3 calificaciones</h1>
-	<input type="number" name="nota1" min="0" placeholder="1° Calificación" required> * 30<br><br>
-	<input type="number" name="nota2" min="0" placeholder="2° Calificación" required> * 20<br><br>
-	<input type="number" name="nota3" min="0" placeholder="3° Calificación" required> * 10<br><br>
-	<input type="submit" name="btnresultado" value="Calcular">
-</form>
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Calculadora de Calificaciones y Salarios</title>
+		<link rel="icon" href="../../assets/img/logo.png">
+		<link rel="apple-touch-icon" href="../../assets/img/logo-grande.png">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<form method="POST" name="AlejandroVillegas">
+			<h1>Calificaciones</h1>
+			<input type="number" name="nota1" min="0" placeholder="Calificación 1" required> * 30<br><br>
+			<input type="number" name="nota2" min="0" placeholder="Calificación 2" required> * 20<br><br>
+			<input type="number" name="nota3" min="0" placeholder="Calificación 3" required> * 10<br><br>
+			<input type="submit" name="btnresultado" value="Calcular">
+		</form>
 
-<?php
+		<?php
+		if (isset($_POST['btnresultado'])) {
+			$var1 = $_POST['nota1'];
+			$var2 = $_POST['nota2'];
+			$var3 = $_POST['nota3'];
 
-if(isset($_POST['btnresultado']))
-{
-	$var1 = $_POST['nota1'];
-	$var2 = $_POST['nota2'];
-	$var3 = $_POST['nota3'];
-	
-	echo "<h1>Resultado</h1>";
-	echo "1° Calificación: $var1 por 30: ".($var1 * 30)." <br><br>";
-	echo "2° Calificación: $var2 por 20: ".($var2 * 20)."<br><br>";
-	echo "3° Calificación: $var3 por 10: ".($var3 * 10)."<br><br>";
-	
-}
-
-?>
+			echo "<h2>Resultado</h2>";
+			echo "<p>Calificación 1: $var1 * 30 = " . ($var1 * 30) . "</p>";
+			echo "<p>Calificación 2: $var2 * 20 = " . ($var2 * 20) . "</p>";
+			echo "<p>Calificación 3: $var3 * 10 = " . ($var3 * 10) . "</p>";
+		}
+		?>
+	</body>
+</html>
--- ejercicio02.php
+++ ejercicio02.php
@@ -1,28 +1,32 @@
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css">
-</head>
-<form method="POST" name="AlejandroVillegas">
-<h1>Salario de un empleado</h1>
-<label>Si este trabaja 8 horas diarias y gana 30 pesos la hora.</label><br><br>
-<label>¿Cuánto sería en un día?</label><br><br>
-<label>¿Cuánto sería en una semana?</label><br><br>
-<label>¿Cuánto sería en 15 días?</label><br><br>
-<input type="submit" name="btncalcular" value="Calcular">
-</form>
-
-<?php
-
-if(isset($_POST['btncalcular']))
-{
-	$salariodeldia = 8 * 30;
-	$salariodelasemana = $salariodeldia*7;
-	$salarioen15dias = $salariodeldia * 15;
-	
-	echo "<h1>Resultado</h1>";
-	echo "Salario de un día: $salariodeldia <br><br>";
-	echo "Salario en una semana: $salariodelasemana<br><br>";
-	echo "Salario en 15 días: $salarioen15dias <br><br>";
-}
-
-?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Calculadora de Calificaciones y Salarios</title>
+		<link rel="icon" href="../../assets/img/logo.png">
+		<link rel="apple-touch-icon" href="../../assets/img/logo-grande.png">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<form method="POST" name="AlejandroVillegas">
+			<h1>Salario de un empleado</h1>
+			<label>Si este trabaja 8 horas diarias y gana 30 pesos la hora.</label><br><br>
+			<label>¿Cuánto sería en un día?</label><br><br>
+			<label>¿Cuánto sería en una semana?</label><br><br>
+			<label>¿Cuánto sería en 15 días?</label><br><br>
+			<input type="submit" name="btncalcular" value="Calcular">
+		</form>
+		<?php
+		if (isset($_POST['btncalcular'])) {
+			$salariodeldia = 8 * 30;
+			$salariodelasemana = $salariodeldia * 7;
+			$salarioen15dias = $salariodeldia * 15;
+			echo "<h2>Resultado</h2>";
+			echo "<p>Salario de un día: $salariodeldia</p>";
+			echo "<p>Salario en una semana: $salariodelasemana</p>";
+			echo "<p>Salario en 15 días: $salarioen15dias</p>";
+		}
+		?>
+	</body>
+</html>
